39883bac3046769f740faec43faa5bd37e06a634
tnbozman <tnbozman@gmail.com>
2023-11-09 08:58:14 +1030

    feat: scaffolding

---
+++ src/test/dewc/com/microservice_example/MicroserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dewc.com.microservice_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
