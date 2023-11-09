4b382eb319f1f2d10c9d187846e34cbbc476a964
TNBOZMAN <tnbozman@gmail.com>
2023-11-09 02:54:03 +0000

    feat: improved the item dto

--- src/main/java/dewc/com/microservice_example/controllers/dtos/ItemDTO.java
+++ src/main/java/dewc/com/microservice_example/controllers/dtos/ItemDTO.java
@@ -7,25 +7,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemDTO {
+public class ItemDTO extends ItemCreateDTO {
 
     @NotNull(message = "ID cannot be null")
     private String id;
 
-    @NotBlank(message = "Label is required")
-    @Length(max = 100, message = "Label must be less than 100 characters")
-    @JsonProperty("label") // This is actually redundant unless you want to change the name
-    private String label;
-
-    @NotBlank(message = "Description is required")
-    @Length(max = 500, message = "Description must be less than 500 characters")
-    @JsonProperty("description") // This is actually redundant unless you want to change the name
-    private String description;
 
 }
